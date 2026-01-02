c964cf4f2736768f5358c7e598e5343b01726c5e
Niccolò Perego <perego.nicco@gmail.com>
2026-01-02 04:05:08 +0100

    feat: add wpm & accuracy; redirect to results on finish

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { BehaviorSubject, distinctUntilChanged, filter, fromEvent, iif, map, of, scan, Subject, switchMap, takeUntil, takeWhile, tap, timer, withLatestFrom } from "rxjs";
 import data from "./../../data.json";
 
@@ -8,7 +9,8 @@ type Difficulty = keyof typeof data;
 type Mode = "TIMED" | "PASSAGE";
 interface CharStateI {
   value: string;
-  state: "PENDING" | "CORRECT" | "INCORRECT"
+  state: "PENDING" | "CORRECT" | "INCORRECT";
+  historicalError: boolean;
 }
 @Component({
   selector: 'tst-home',
@@ -27,6 +29,7 @@ export class Home implements AfterViewInit, OnDestroy {
   readonly #MAX_CHAR_CODE = 126;
   readonly #COUNTDOWN_START_AT = 60;
   #randomTextIndex = 0;
+  #startTime: number | null = null;
 
   readonly onDestroy$ = new Subject<void>();
   readonly testText$ = new BehaviorSubject<CharStateI[]>([]);
@@ -34,15 +37,21 @@ export class Home implements AfterViewInit, OnDestroy {
   readonly mode$ = new BehaviorSubject<Mode>(this.mode);
   readonly testStarted$ = new BehaviorSubject<boolean>(false);
   readonly countdown$ = this.testStarted$.pipe(
+    distinctUntilChanged(),
     switchMap(testStarted => iif(
       () => testStarted,
       timer(0, 1000).pipe(
         scan((acc) => acc - 1, this.#COUNTDOWN_START_AT + 1),
-        takeWhile((countdown) => countdown >= 0)
+        takeWhile((countdown) => countdown >= 0),
+        tap(countdown => {
+          if (countdown === 0) {
+            this._router.navigateByUrl("/result");
+          }
+        })
       ),
       of(this.#COUNTDOWN_START_AT)
     )),
-    map(this.#secondsToStringTime)
+    map(this.#secondsToStringTime),
   );
   readonly infiniteTimer$ = this.testStarted$.pipe(
     switchMap(testStarted => iif(
@@ -59,6 +68,21 @@ export class Home implements AfterViewInit, OnDestroy {
       this.infiniteTimer$
     ))
   );
+  readonly wpm$ = new BehaviorSubject<number>(0);
+
+  #getAccuracy = (testText: CharStateI[]) => {
+    const correctChars = testText.length - this.#getWrongCharsCount(testText);
+
+    if (correctChars === 0) return 0;
+
+    return Math.round((correctChars * 100) / testText.length)
+  }
+
+  readonly accuracy$ = this.testText$.pipe(
+    map(this.#getAccuracy)
+  );
+
+  constructor(private _router: Router) { }
 
   ngAfterViewInit(): void {
     this.#subscribeSettingsFormValueChanges();
@@ -126,17 +150,29 @@ export class Home implements AfterViewInit, OnDestroy {
     const isBackspace = event.code === "Backspace";
 
     if (testStarted) {
-      if (event.key === testText[cursorPosition].value) {
-        testText[cursorPosition].state = "CORRECT";
-      } else if (isBackspace) {
-        // TODO: HANDLE BACKSPACE
-        // this.testText$.next()
-      } else {
-        testText[cursorPosition].state = "INCORRECT";
+      const keyIsCorrect = testText[cursorPosition].value === event.key;
+
+      testText[cursorPosition].state = keyIsCorrect ? "CORRECT" : "INCORRECT";
+
+      if (!keyIsCorrect) {
+        testText[cursorPosition].historicalError = true;
       }
 
-      if (cursorPosition >= testText.length - 1) return;
+      if (cursorPosition >= testText.length - 1) this._router.navigateByUrl("/result");
       if (isBackspace) return this.cursorPosition$.next(Math.max(cursorPosition - 1, 0));
+
+      if (this.#startTime === null) {
+        this.#startTime = performance.now();
+      }
+
+      const currentTime = performance.now();
+
+      if (currentTime - this.#startTime > 0) {
+        const completedChars = this.#getCompletedChars(testText);
+        const wpm = this.#calculateWpm(completedChars, this.#startTime, currentTime);
+
+        this.wpm$.next(wpm);
+      }
 
       this.cursorPosition$.next(cursorPosition + 1);
       this.testText$.next(testText);
@@ -146,7 +182,7 @@ export class Home implements AfterViewInit, OnDestroy {
   }
 
   #getCharsState(text: string): CharStateI[] {
-    return text.split("").map(char => ({ value: char, state: "PENDING" }));
+    return text.split("").map(char => ({ value: char, state: "PENDING", historicalError: false }));
   }
 
   #secondsToStringTime(seconds: number): string {
@@ -158,5 +194,30 @@ export class Home implements AfterViewInit, OnDestroy {
 
   #getRandomIndex(max: number) {
     return Math.floor(Math.random() * max);
+  }
+
+  #calculateWpm(charCount: number, startTime: number, endTime: number): number {
+    const timeInMs = endTime - startTime;
+    const timeInMinutes = timeInMs / (1000 * 60);
+    const STD_WORD_CHARS = 5;
+    const wordCount = charCount / STD_WORD_CHARS;
+
+    return Math.round(wordCount / timeInMinutes);
+  }
+
+  #getFirstPendingCharIndex(testText: CharStateI[]): number {
+    return testText.findIndex(char => char.state === "PENDING");
+  }
+
+  #getCompletedChars(testText: CharStateI[]): number {
+    if (this.#getFirstPendingCharIndex(testText) === -1) {
+      return Math.min(this.#getFirstPendingCharIndex(testText) + 1, testText.length);
+    }
+
+    return this.#getFirstPendingCharIndex(testText);
+  }
+
+  #getWrongCharsCount(testText: CharStateI[]): number {
+    return testText.filter(char => char.historicalError).length;
   }
 }
