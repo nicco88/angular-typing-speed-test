97a1232a0ee00f8009fe8ede34d7b147d4c1acc8
Niccolò Perego <perego.nicco@gmail.com>
2026-01-09 17:16:22 +0100

    fix: improve buttons state styles

--- src/app/directives/button.ts
+++ src/app/directives/button.ts
@@ -5,15 +5,15 @@ type Variant = "primary" | "secondary" | "tertiary";
 @Directive({
   selector: 'a[data-tstButton],button[data-tstButton]',
   host: {
-    "class": "cursor-pointer px-6 py-4 rounded-[12px] text-xl font-semibold inline-flex items-center justify-center gap-x-2.5 transition-all duration-300 focus:outline-4 outline-offset-2 outline-neutral-0",
+    "class": "cursor-pointer px-6 py-4 rounded-[12px] text-xl font-semibold inline-flex items-center justify-center gap-x-2.5 transition-all duration-300 focus:outline-4 outline-offset-3 outline-blue-400",
     "[class]": "classes[variant()]",
   },
 })
 export class Button {
   classes: Record<Variant, string> = {
-    primary: "bg-blue-600 hover:bg-neutral-0 hover:text-blue-600",
-    secondary: "bg-neutral-800 hover:bg-neutral-700",
-    tertiary: "bg-neutral-0 text-neutral-900 hover:bg-neutral-200",
+    primary: "bg-blue-600 hover:bg-blue-400",
+    secondary: "bg-neutral-0 text-neutral-900 hover:bg-neutral-0/90",
+    tertiary: "bg-neutral-800 hover:bg-neutral-700",
   }
   variant = input<Variant>("primary");
 }
--- src/app/directives/settings-option.ts
+++ src/app/directives/settings-option.ts
@@ -4,19 +4,25 @@ import { Directive, input } from '@angular/core';
   selector: 'label[data-tstSettingsOption],button[data-tstSettingsOption]',
   host: {
     "[attr.tabindex]": "0",
-    "class": "cursor-pointer px-2 py-1 rounded-lg border transition-all duration-200 inline-flex justify-center items-center gap-x-2.5 focus:outline-2 focus:outline-neutral-0 focus:outline-offset-2",
-    "[class.border-neutral-400]": "!active()",
-    "[class.hover:bg-neutral-0]": "!active()",
-    "[class.hover:text-neutral-800]": "!active()",
-    "[class.focus:outline-neutral-0]": "!active()",
-
-    "[class.border-blue-400]": "active()",
-    "[class.text-blue-400]": "active()",
-    "[class.hover:bg-blue-400]": "active()",
-    "[class.hover:text-blue-900]": "active()",
-    "[class.focus:outline-blue-400]": "active()",
+    "[attr.aria-description]": "'Press enter to select this option'",
+    "class": "cursor-pointer px-2 py-1 rounded-lg border transition-all duration-200 inline-flex justify-center items-center gap-x-2.5 focus:outline-2 focus:outline-offset-2 focus:outline-blue-400",
+    "[class]": "active() ? classes.active : classes.inactive",
+    "(keydown.enter)": "onEnter($event)"
   }
 })
 export class SettingsOption {
   active = input.required<boolean>();
+  classes = {
+    active: "border-blue-400 text-blue-400 hover:bg-blue-400 hover:text-blue-900",
+    inactive: "border-neutral-400 hover:bg-neutral-0 hover:text-neutral-800",
+  }
+
+  onEnter(event: Event) {
+    event.preventDefault();
+
+    const target = event.target as HTMLElement;
+
+    target.click();
+    target.focus();
+  }
 }
